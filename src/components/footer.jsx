2d7cabe91c78b1ba0ae169697fedd1ad9792a29d
shaamiilll <shamilamiyan@gmail.com>
2025-10-13 23:24:42 +0530

    UPDATE:Updated the footer with the link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/logo.jpg';
+// import logo from '../assets/logo.jpg';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -8,13 +8,19 @@ const Footer = () => {
     <footer className="bg-white border-t border-gray-200">
       <div className="container mx-auto px-6 py-4">
         <div className="flex items-center justify-center gap-3">
-          <img 
-            src={logo} 
-            alt="Aurify Technologies Logo" 
-            className="h-10 w-20 object-contain"
-          />
-          <span className="text-blue-600 font-semibold text-sm">
-            © {currentYear} Powered by Aurify Technologies. All rights reserved.
+
+          <span className="text-gray-700  text-sm hover:cursor-pointer">
+            © {currentYear} Powered by{' '}
+            <a
+
+              href="https://aurify.ae"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              Aurify Technologies
+            </a>
+            . All rights reserved.
           </span>
         </div>
       </div>
